0678902f606340f74c7ff9bad2d1df16140ba079
Sergey <seergoo117@gmail.com>
2018-06-08 12:12:26 +0300

    Finish toy app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    # find the user record
+    @user = User.find(params[:id])
+    @micropost = @user.microposts.first
   end
 
   # GET /users/new
